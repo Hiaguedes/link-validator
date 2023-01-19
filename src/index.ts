0fe575263342a616bd9cff6b571b295c90389f02
Hiaguedes <hiago@contasimples.com>
2023-01-19 14:23:44 -0300

    chore: extracao dos links no arquivo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import chalk from 'chalk'
 import { promises, readFileSync } from 'fs'
 import { resolve } from 'path'
+import { tuppleNameAndLinkArr } from './link-pattern.js'
 import log, { colors } from './log-colors.js'
-
 
 const readUTF8File = (path: string) => {
     console.log(chalk.magenta(`lendo arquivo que esta em ${resolve(path)}`));
@@ -27,7 +27,11 @@ const asyncReadUTF8File = async (path: string) => {
 }
 
 try {
-    asyncReadUTF8File('src/arquivos_exemplo/file.md');
+    (async() => {
+        const text = await asyncReadUTF8File('src/arquivos_exemplo/file.md');
+        console.log(colors.yellow(tuppleNameAndLinkArr(text).links))
+
+    })()
 } catch (e){
     console.error(e)
 }
---
+++ src/link-pattern.ts
@@ -0,0 +1,24 @@
+const LinkPattern = /\[(.*?)\]\((https?:\/\/.*?)\)/g
+
+const identifyMatchesOnMarkdownFile = (text: string) => {
+    return text.match(LinkPattern);
+}
+
+const tuppleNameAndLinkArr = (text: string) => {
+    let links = [];
+    let names = [];
+    let raw = []
+    let match;
+    while((match = LinkPattern.exec(text)) != null){
+        raw.push([match[1], match[2]]);
+        names.push(match[1]);
+        links.push(match[2]);
+    }
+    return {
+        links,
+        names,
+        raw,
+    };
+}
+
+export { identifyMatchesOnMarkdownFile, tuppleNameAndLinkArr }
